c51b89a4795cddb60ae6cfb73a083f10ee1dcc88
Mason Keresty <89534948+mkeresty@users.noreply.github.com>
2023-11-16 21:09:44 -0500

    Create about.js

---
+++ about.js
@@ -0,0 +1,235 @@
+const aboutInfo = 
+(
+        [
+            {
+                "_id": "6432bd33fd88a2d9e3288e48",
+                "aboutText": "Hello there! I'm Mason, a full stack developer and a combat systems engineer, building with innovation and design",
+                "section": "about",
+                "aboutImage": "",
+                "aboutHoverImage": ""
+            },
+            {
+                "_id": "6432c66de600088d794f1e91",
+                "section": "hero",
+                "typewriter": [
+                    "Hey this is Mason!",
+                    "fullStackDeveloper.js",
+                    "Engineer 🚢",
+                    "<Creator/> "
+                ],
+                "title": "Engineer | Fullstack Developer",
+                "heroImage": "https://media.licdn.com/dms/image/C5603AQE73f6RnFt13g/profile-displayphoto-shrink_800_800/0/1628866566516?e=2147483647&v=beta&t=ITrUm2C--WynTVj367QIMw2kN8UabaO8vy3gepoI5sY"
+            },
+            {
+                "_id": "6432c881e600088d794f1e94",
+                "section": "experience",
+                "certData": [
+                    {
+                        "name": "Lockheed Martin",
+                        "link": "https://www.lockheedmartin.com/",
+                        "provider": "New Jersey",
+                        "offeredBy": "Developer | Engineer",
+                        "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSC-0_pLqAY_MwxF1_6Zd2C7WUomWVQQVFemS7MKHEDyCaUGlDFRmg9IGpqkvOrA9ODIUs&usqp=CAU",
+                        "status": "2022 - Present",
+                        "learnPoints": [
+                            "Combat Systems Engineering",
+                            "Fullstack Development",
+                            "Data Structures",
+                            "Machine Learning"
+                        ]
+                    },
+                    {
+                        "name": "STF Technologies",
+                        "link": "https://www.stf-technologies.com/",
+                        "provider": "Delaware",
+                        "offeredBy": "Research Engineer",
+                        "logo": "https://www.stf-technologies.com/wp-content/uploads/cropped-Picture2-e1596810925438.png",
+                        "status": "2020 - 2022",
+                        "learnPoints": [
+                            "Nuclear Rheology",
+                            "Equipment Design",
+                            "Software Development",
+                            "Experimentation"
+                        ]
+                    },
+                    {
+                        "name": "Air Liquide",
+                        "link": "https://www.airliquide.com/",
+                        "provider": "Delaware",
+                        "offeredBy": "Process Engineering Intern",
+                        "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRZQXhiVpLQ_piixXKzxZg2zokMQXjkNGUNxdydD5eS4w&s",
+                        "status": "2019 - 2020",
+                        "learnPoints": [
+                            "Manufacturing Optimization",
+                            "Aircraft Fuel Systems",
+                            "Gas Filtering Devices",
+                        ]
+                    },
+                    {
+                        "name": "EDiS",
+                        "link": "https://www.ediscompany.com/",
+                        "provider": "Delaware",
+                        "offeredBy": "Project Management Intern",
+                        "logo": "https://www.ediscompany.com/images/logo.svg",
+                        "status": "20129 - 2019",
+                        "learnPoints": [
+                            "Software Optimization",
+                            "Construction Management",
+                            "Blueprints",
+                        ]
+                    },
+                ]
+            },
+            {
+                "_id": "6432cb77e600088d794f1e96",
+                "section": "skills",
+                "skillData": [
+                    {
+                        "skillName": "javascript",
+                        "proficiency": "85",
+                        "imageLink": "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
+                    },
+                    {
+                        "skillName": "react",
+                        "proficiency": "90",
+                        "imageLink": "https://i.ibb.co/YyGmCM0/react.png"
+                    },
+                    {
+                        "skillName": "Tailwind",
+                        "proficiency": "90",
+                        "imageLink": "https://i.ibb.co/mD3rstb/z8hzeszc9eb3sp3vp3qc.jpg"
+                    },
+                    {
+                        "skillName": "next.js",
+                        "proficiency": "85",
+                        "imageLink": "https://i.ibb.co/3vDj7JL/Next-js-900x0.png"
+                    },
+                    {
+                        "skillName": "node.js",
+                        "proficiency": "85",
+                        "imageLink": "https://i.ibb.co/rQS8r4P/node-js.png"
+                    },
+                    {
+                        "skillName": "express.js",
+                        "proficiency": "80",
+                        "imageLink": "https://i.ibb.co/MBHgZ74/expressjs-logo-icon-169185.png"
+                    },
+                    {
+                        "skillName": "solidity",
+                        "proficiency": "75",
+                        "imageLink": "https://docs.soliditylang.org/en/latest/_images/solidity_logo.svg"
+                    },
+                    {
+                        "skillName": "python",
+                        "proficiency": "85",
+                        "imageLink": "https://i.ibb.co/mBKKN4S/python-logo-4k-i6-1600x1200.jpg"
+                    },
+                    {
+                        "skillName": "c#",
+                        "proficiency": "70",
+                        "imageLink": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/C_Sharp_Logo_2023.svg/192px-C_Sharp_Logo_2023.svg.png"
+                    },
+                    {
+                        "skillName": "Unity",
+                        "proficiency": "75",
+                        "imageLink": "https://assets.stickpng.com/images/62e131df7fe3599fdd46ecb3.png"
+                    },
+                    {
+                        "skillName": "mongodb",
+                        "proficiency": "80",
+                        "imageLink": "https://res.cloudinary.com/crunchbase-production/image/upload/c_lpad,f_auto,q_auto:eco,dpr_1/erkxwhl1gd48xfhe2yld"
+                    },
+                    {
+                        "skillName": "mysql",
+                        "proficiency": "80",
+                        "imageLink": "https://pngimg.com/d/mysql_PNG9.png"
+                    },
+                    {
+                        "skillName": "Elasticsearch",
+                        "proficiency": "80",
+                        "imageLink": "https://sue.eu/wp-content/uploads/sites/6/2022/07/elastic-logo-920x920-sue-v02.png"
+                    },
+                    {
+                        "skillName": "GraphQL",
+                        "proficiency": "75",
+                        "imageLink": "https://www.ibm.com/content/dam/adobe-cms/instana/media_logo/GraphQL-Java.png/_jcr_content/renditions/cq5dam.web.1280.1280.png"
+                    },
+                    {
+                        "skillName": "AWS",
+                        "proficiency": "70",
+                        "imageLink": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7PB7gDdA6O7gpwXX-cDoelORPic-1rSP6xRUfJQX_zw&s"
+                    },
+                    {
+                        "skillName": "Firebase",
+                        "proficiency": "85",
+                        "imageLink": "https://cdn.dribbble.com/users/528264/screenshots/3140440/firebase_logo_1x.png"
+                    },
+                ]
+            },
+            {
+                "_id": "6432d818e600088d794f1e97",
+                "section": "projects",
+                "projectsData": [
+                    {
+                        "projectName": "Linagee Forever",
+                        "projectDesc": "Linagee Forever is a cutting-edge web application designed for the seamless management of domain names derived from the oldest NFT contract on the Ethereum blockchain. Developed using SolidJS, this innovative platform leverages the power of the Graph Network for efficient data retrieval and is entirely stored on the Ethereum blockchain, and accessed through a lightweight React app. Linagee Forever provides decentralized domain management on Ethereum, offering users robust control in the blockchain domain landscape.",
+                        "projectImage": "lnrforever.png",
+                        "projectLink": "https://web.linagee.vision/og/lnrforever.og",
+                        "projectDuration": ""
+                    },
+                    {
+                        "projectName": "StampVerse",
+                        "projectDesc": "StampVerse, powered by Next.js, facilitates exploration, creation, and purchase of Bitcoin blockchain-based NFTs known as stamps, offering a seamless experience for enthusiasts and collectors alike.",
+                        "projectImage": "stampverse.jpg",
+                        "projectLink": "https://stampverse.io/",
+                        "projectDuration": ""
+                    },
+                    {
+                        "projectName": "Linagee.vision",
+                        "projectDesc": "Linagee.vision, a platform born out of the discovery of the oldest NFT contract on Ethereum by me in 2022. The website empowers users to claim domain names for free, fostering a decentralized digital space. Beyond claiming, users can effortlessly transfer names and utilize a custom smart contract wrapper for seamless wrap and unwrap operations.",
+                        "projectImage": "https://linagee.vision/",
+                        "projectLink": "/linagee.vision.jpg",
+                        "projectDuration": ""
+                    },
+                    {
+                        "projectName": "Data Management System",
+                        "projectDesc": "A Data Management System made with NextJS designed to streamline navigation through vast datasets—unlocking significant time savings for users dealing with both new and legacy data.",
+                        "projectImage": "/dbm.png",
+                        "projectLink": "",
+                        "projectDuration": ""
+                    }
+                ]
+            },
+            {
+                "_id": "6432d9f6e600088d794f1e98",
+                "section": "header",
+                "linkedin": "https://www.linkedin.com/in/mason-keresty",
+                "github": "https://github.com/mkeresty",
+            },
+            {
+                "_id": "6432c881e6583934494f1e93",
+                "section": "patents",
+                "patentData": [
+                    {
+                        "name": "Rheology",
+                        "link": "https://pamspublic.science.energy.gov/WebPAMSExternal/Interface/Common/ViewPublicAbstract.aspx?rv=e20817f9-c77f-4877-9cc9-7f5d2c05ab6c&rtc=24&PRoleId=10",
+                        "provider": "NEUTRON REFLECTIVITY-RHEOLOGY SAMPLE ENVIRONMENT FOR INVESTIGATING MATERIALS AT AIR-LIQUID AND LIQUID-LIQUID INTERFACES AND METHODS OF USE THEREOF",
+                        "offeredBy": "United States Patent Application No. 63/349,961",
+                        "logo": "https://www.stf-technologies.com/wp-content/uploads/cropped-Picture2-e1596810925438.png",
+                        "status": "2020 - 2023",
+                        "photos": ["https://cbe.udel.edu/wp-content/uploads/2021/11/11182021_Surface-Exploration.jpg", "https://cbe.udel.edu/wp-content/uploads/2021/11/11182021_Surface-Exploration_v2.jpg"],
+                        "description": "We developed an innovative neutron rhometer, RheoSurfRTM, addressing a crucial gap in scientific measurement capabilities for complex fluid interfaces. This sample environment, integrated with cutting-edge techniques like interfacial rheology and microscopy, provides simultaneous measurement of surface moduli and molecular structures at various length scales. The prototype, featuring a quad-motor system and neutron-transparent interface containment, successfully demonstrated controlled interfacial deformation and sample thermodynamic state adjustments.",
+                        "learnPoints": [
+                            "Interfacial Rheology",
+                            "Precision Motion Control",
+                            "Neutron Beamline Experiments",
+                        ],
+
+                    },
+                ]
+            },
+
+        ]
+    )
+export default aboutInfo;
+
